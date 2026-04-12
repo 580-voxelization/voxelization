c575f215b7bd99daec8f5bb74a7ecabbc823e7e9
gbccccc <gbccccc233@gmail.com>
2026-04-11 19:10:21 -0700

    Add parsing branch for VoxelizedMesh
    Add parsing branch for VoxelizedMesh

--- parse.cpp
+++ parse.cpp
@@ -9,6 +9,7 @@
 #include "reflective_shader.h"
 #include "render_world.h"
 #include "sphere.h"
+#include "voxelized_mesh.h"
 #include <map>
 #include <sstream>
 #include <string>
@@ -16,146 +17,154 @@
 
 //This ray-tracer uses a custom scene file format that is human readable
 //It is lacking heirachial information and its much less descriptively powerfull than .usd or .gltf
-void Parse(Render_World& world,int& width,int& height,const char* test_file)
+void Parse(Render_World &world, int &width, int &height, const char *test_file)
 {
     std::string input_path = "input/" + std::string(test_file);
-    FILE* F = fopen(input_path.c_str(),"r");
-    if(!F)
+    FILE *F = fopen(input_path.c_str(), "r");
+    if (!F)
     {
-        std::cout<<"Failed to open file '"<<test_file<<"'\n";
+        std::cout << "Failed to open file '" << test_file << "'\n";
         exit(EXIT_FAILURE);
     }
 
     double f0;
     char buff[1000];
-    vec3 u,v,w;
-    std::string s0,s1,s2,mat;
+    vec3 u, v, w;
+    std::string s0, s1, s2, mat;
 
-    std::map<std::string,vec3> colors;
-    std::map<std::string,Shader*> shaders;
-    shaders["-"]=0;
+    std::map<std::string, vec3> colors;
+    std::map<std::string, Shader *> shaders;
+    shaders["-"] = 0;
 
-    auto finish_parse_object=[&](Object* o)
+    auto finish_parse_object = [&](Object *o)
     {
-        std::map<std::string,Shader*>::const_iterator sh=shaders.find(mat);
-        assert(sh!=shaders.end());
-        o->material_shader=sh->second;
+        std::map<std::string, Shader *>::const_iterator sh = shaders.find(mat);
+        assert(sh != shaders.end());
+        o->material_shader = sh->second;
         world.objects.push_back(o);
     };
 
-    while(fgets(buff, sizeof(buff), F))
+    while (fgets(buff, sizeof(buff), F))
     {
         std::stringstream ss(buff);
-        std::string item,name;
-        if(!(ss>>item) || !item.size() || item[0]=='#') continue;
-        if(item=="size")
+        std::string item, name;
+        if (!(ss >> item) || !item.size() || item[0] == '#') continue;
+        if (item == "size")
         {
-            ss>>width>>height;
+            ss >> width >> height;
             assert(ss);
         }
-        else if(item=="color")
+        else if (item == "color")
         {
-            ss>>name>>u;
+            ss >> name >> u;
             assert(ss);
-            colors[name]=u;
+            colors[name] = u;
         }
-        else if(item=="plane")
+        else if (item == "plane")
         {
-            ss>>u>>v>>mat;
+            ss >> u >> v >> mat;
             assert(ss);
-            finish_parse_object(new Plane(u,v));
+            finish_parse_object(new Plane(u, v));
         }
-        else if(item=="sphere")
+        else if (item == "sphere")
         {
-            ss>>u>>f0>>mat;
+            ss >> u >> f0 >> mat;
             assert(ss);
-            finish_parse_object(new Sphere(u,f0));
+            finish_parse_object(new Sphere(u, f0));
         }
-        else if(item=="mesh")
+        else if (item == "mesh")
         {
-            ss>>s0>>mat;
+            ss >> s0 >> mat;
             assert(ss);
-            Mesh* o=new Mesh;
+            Mesh *o = new Mesh;
             o->Read_Obj(s0.c_str());
             finish_parse_object(o);
         }
-        else if(item=="flat_shader")
+        else if (item == "voxelized_mesh")
         {
-            ss>>name>>s0;
+            ss >> s0 >> mat;
             assert(ss);
-            std::map<std::string,vec3>::const_iterator c0=colors.find(s0);
-            assert(c0!=colors.end());
-            shaders[name]=new Flat_Shader(world,c0->second);
+            VoxelizedMesh *o = new VoxelizedMesh;
+            o->Read_Obj(s0.c_str());
+            finish_parse_object(o);
         }
-        else if(item=="phong_shader")
+        else if (item == "flat_shader")
         {
-            ss>>name>>s0>>s1>>s2>>f0;
+            ss >> name >> s0;
             assert(ss);
-            std::map<std::string,vec3>::const_iterator c0=colors.find(s0);
-            std::map<std::string,vec3>::const_iterator c1=colors.find(s1);
-            std::map<std::string,vec3>::const_iterator c2=colors.find(s2);
-            assert(c0!=colors.end());
-            assert(c1!=colors.end());
-            assert(c2!=colors.end());
-            shaders[name]=new Phong_Shader(world,c0->second,c1->second,c2->second,f0);
+            std::map<std::string, vec3>::const_iterator c0 = colors.find(s0);
+            assert(c0 != colors.end());
+            shaders[name] = new Flat_Shader(world, c0->second);
         }
-        else if(item=="reflective_shader")
+        else if (item == "phong_shader")
         {
-            ss>>name>>s0>>f0;
+            ss >> name >> s0 >> s1 >> s2 >> f0;
             assert(ss);
-            std::map<std::string,Shader*>::const_iterator sh=shaders.find(s0);
-            assert(sh!=shaders.end());
-            shaders[name]=new Reflective_Shader(world,sh->second,f0);
+            std::map<std::string, vec3>::const_iterator c0 = colors.find(s0);
+            std::map<std::string, vec3>::const_iterator c1 = colors.find(s1);
+            std::map<std::string, vec3>::const_iterator c2 = colors.find(s2);
+            assert(c0 != colors.end());
+            assert(c1 != colors.end());
+            assert(c2 != colors.end());
+            shaders[name] = new Phong_Shader(world, c0->second, c1->second, c2->second, f0);
         }
-        else if(item=="point_light")
+        else if (item == "reflective_shader")
         {
-            ss>>u>>s0>>f0;
+            ss >> name >> s0 >> f0;
             assert(ss);
-            std::map<std::string,vec3>::const_iterator c0=colors.find(s0);
-            assert(c0!=colors.end());
-            world.lights.push_back(new Point_Light(u,c0->second,f0));
+            std::map<std::string, Shader *>::const_iterator sh = shaders.find(s0);
+            assert(sh != shaders.end());
+            shaders[name] = new Reflective_Shader(world, sh->second, f0);
         }
-        else if(item=="ambient_light")
+        else if (item == "point_light")
         {
-            ss>>s0>>f0;
+            ss >> u >> s0 >> f0;
             assert(ss);
-            std::map<std::string,vec3>::const_iterator c0=colors.find(s0);
-            assert(c0!=colors.end());
-            world.ambient_color=c0->second;
-            world.ambient_intensity=f0;
+            std::map<std::string, vec3>::const_iterator c0 = colors.find(s0);
+            assert(c0 != colors.end());
+            world.lights.push_back(new Point_Light(u, c0->second, f0));
         }
-        else if(item=="camera")
+        else if (item == "ambient_light")
         {
-            ss>>u>>v>>w>>f0;
+            ss >> s0 >> f0;
             assert(ss);
-            world.camera.Position_And_Aim_Camera(u,v,w);
-            world.camera.Focus_Camera(1,(double)width/height,f0*(pi/180));
+            std::map<std::string, vec3>::const_iterator c0 = colors.find(s0);
+            assert(c0 != colors.end());
+            world.ambient_color = c0->second;
+            world.ambient_intensity = f0;
         }
-        else if(item=="background")
+        else if (item == "camera")
         {
-            ss>>s0;
+            ss >> u >> v >> w >> f0;
             assert(ss);
-            std::map<std::string,Shader*>::const_iterator sh=shaders.find(s0);
-            assert(sh!=shaders.end());
-            world.background_shader=sh->second;
+            world.camera.Position_And_Aim_Camera(u, v, w);
+            world.camera.Focus_Camera(1, (double) width / height, f0 * (pi / 180));
         }
-        else if(item=="enable_shadows")
+        else if (item == "background")
         {
-            ss>>world.enable_shadows;
+            ss >> s0;
+            assert(ss);
+            std::map<std::string, Shader *>::const_iterator sh = shaders.find(s0);
+            assert(sh != shaders.end());
+            world.background_shader = sh->second;
+        }
+        else if (item == "enable_shadows")
+        {
+            ss >> world.enable_shadows;
             assert(ss);
         }
-        else if(item=="recursion_depth_limit")
+        else if (item == "recursion_depth_limit")
         {
-            ss>>world.recursion_depth_limit;
+            ss >> world.recursion_depth_limit;
             assert(ss);
         }
         else
         {
-            std::cout<<"Failed to parse: "<<buff<<std::endl;
+            std::cout << "Failed to parse: " << buff << std::endl;
             exit(EXIT_FAILURE);
         }
     }
-    if(!world.background_shader)
-        world.background_shader=new Flat_Shader(world,vec3());
-    world.camera.Set_Resolution(ivec2(width,height));
+    if (!world.background_shader)
+        world.background_shader = new Flat_Shader(world, vec3());
+    world.camera.Set_Resolution(ivec2(width, height));
 }
